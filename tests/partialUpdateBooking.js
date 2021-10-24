eb17ba5b5a4ea7915d3506c76fe879eba29459b9
Anikó Borosová <borosanikoo@gmail.com>
2021-10-24 13:00:05 +0200

    partialUpdate tests

--- tests/partialUpdateBooking.js
+++ tests/partialUpdateBooking.js
@@ -5,50 +5,147 @@ const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 
 const authValid = require("../testData/testData").authValid;
-const authInvalid = require("../testData/testData").authInvalid;
 const updateBooking = require("../testData/testData").updateBooking;
 const updateBooking2 = require("../testData/testData").updateBooking2;
 const updateBooking3 = require("../testData/testData").updateBooking3;
 const newBookingIllegalValues = require("../testData/testData").newBookingIllegalValues;
 const newBookingMalformedSchema = require("../testData/testData").newBookingMalformedSchema;
 
-describe("Update booking", () => {
+describe("Partial update booking", () => {
 
-    it("updateBooking - positive - update existing booking with partial valid data & accept in 'application/json' format", (done) => {
+	it("partialUpdateBooking - positive - update existing booking with partial valid data & accept in 'application/json' format", (done) => {
 
-        chai
-            .request("http://localhost:3001")
-            .put("/booking/1")
-            .set("Content-Type", "application/json")
-            .set("Accept", "application/json")
-            .auth(authValid.username, authValid.password)
-            .send({
-                "firstname": updateBooking.firstname,
-                "lastname": updateBooking.lastname
-            })
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res).to.have.header("Content-Type", "application/json; charset=utf-8");
-                expect(res).to.have.header("connection", "close");
-                expect(res.body).to.be.an("object");
-                expect(res.body).to.have.property("firstname").that.equals(updateBooking.firstname);
-                expect(res.body).to.have.property("lastname").that.equals(updateBooking.lastname);
-                //expect(res.body).to.have.property("totalprice").that.equals(updateBooking.totalprice);
-                //expect(res.body).to.have.property("depositpaid").that.equals(updateBooking.depositpaid);
-                //expect(res.body).to.have.property("bookingdates").to.have.a.property("checkin").that.equals(updateBooking.bookingdates.checkin);
-                //expect(res.body).to.have.property("bookingdates").to.have.a.property("checkout").that.equals(updateBooking.bookingdates.checkout);
+		chai
+			.request("http://localhost:3001")
+			.patch("/booking/5")
+			.set("Content-Type", "application/json")
+			.set("Accept", "application/json")
+			.auth(authValid.username, authValid.password)
+			.send({
+				"firstname": updateBooking.firstname,
+				"lastname": updateBooking.lastname
+			})
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				expect(res).to.have.header("Content-Type", "application/json; charset=utf-8");
+				expect(res).to.have.header("connection", "close");
+				expect(res.body).to.be.an("object");
+				expect(res.body).to.have.property("firstname").that.equals(updateBooking.firstname);
+				expect(res.body).to.have.property("lastname").that.equals(updateBooking.lastname);
+				done();
+			});
+	});
 
-                //if (res.body.additionalneeds) {
-                //    expect(res.body).to.have.property("additionalneeds").that.equals(updateBooking.additionalneeds);
-                //}
-                done();
-            });
-    });
+	it("partialUpdateBooking - positive - update existing booking with partial valid data & send in text/xml format & accept in 'text/html' format", (done) => {
+
+		chai
+			.request("http://localhost:3001")
+			.patch("/booking/9")
+			.set("Content-Type", "text/xml")
+			.set("Accept", "application/xml")
+			.auth(authValid.username, authValid.password)
+			.send(`<booking> \
+					<totalprice>${updateBooking2.totalprice}</totalprice> \
+					<depositpaid>${updateBooking2.depositpaid}</depositpaid> \
+		 		 </booking>`)
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				expect(res).to.have.header("Content-Type", "text/html; charset=utf-8");
+				expect(res).to.have.header("connection", "close");
+				expect(res.body).to.be.an("object").that.is.empty;
+				expect(res.text).to.have.string("xml");
+				expect(res.text).to.have.string(`${updateBooking2.totalprice}`);
+				expect(res.text).to.have.string(`${updateBooking2.depositpaid}`);
+				done();
+			});
+	});
+
+	it("partialUpdateBooking - negative - update existing booking with partial invalid data - invalid date format", (done) => {
+
+		chai
+			.request("http://localhost:3001")
+			.patch("/booking/10")
+			.set("Content-Type", "application/json")
+			.set("Accept", "application/json")
+			.auth(authValid.username, authValid.password)
+			.send({
+				"bookingdates": newBookingIllegalValues.bookingdates
+			})
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				expect(res).to.have.header("Content-Type", "text/html; charset=utf-8");
+				expect(res).to.have.header("connection", "close");
+				expect(res.body).to.be.an("object").that.is.empty;
+				expect(res.text).to.equal("Invalid date");
+				done();
+			});
+	});
+
+	it("partialUpdateBooking - negative - update existing booking with partial invalid data - numbers as name", (done) => {
+		// creating is not allowed with this lastname, but updating is allowed - might be a design flaw
+
+		chai
+			.request("http://localhost:3001")
+			.patch("/booking/4")
+			.set("Content-Type", "application/json")
+			.set("Accept", "application/json")
+			.auth(authValid.username, authValid.password)
+			.send({
+				"lastname": newBookingIllegalValues.lastname
+			})
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				expect(res).to.have.header("Content-Type", "application/json; charset=utf-8");
+				expect(res).to.have.header("connection", "close");
+				expect(res.body).to.be.an("object");
+				expect(res.body).to.have.property("lastname").that.equals(newBookingIllegalValues.lastname);
+				done();
+			});
+	});
+
+	xit("partialUpdateBooking - negative - update existing booking with malformed data schema", (done) => {
+		// updating is not allowed with this schema, but status code is 200 - might be a design flaw
+
+		chai
+			.request("http://localhost:3001")
+			.patch("/booking/12")
+			.set("Content-Type", "application/json")
+			.set("Accept", "application/json")
+			.auth(authValid.username, authValid.password)
+			.send({
+				"firstname": newBookingMalformedSchema.name,
+				"bookingdates": newBookingMalformedSchema.checkin
+			})
+			.end((err, res) => {
+				expect(res).to.have.status(200);
+				expect(res).to.have.header("Content-Type", "application/json; charset=utf-8");
+				expect(res).to.have.header("connection", "close");
+				expect(res.body).to.be.an("object");
+				expect(res.body).to.have.property("firstname").that.equals(newBookingMalformedSchema.name);
+				expect(res.body).to.have.property("bookingdates").that.equals(newBookingMalformedSchema.checkin);
+				done();
+			});
+	});
+
+	it("partialUpdateBooking - negative - update a non-existing booking with valid partial data", (done) => {
+
+		chai
+			.request("http://localhost:3001")
+			.patch("/booking/991")
+			.set("Content-Type", "application/json")
+			.set("Accept", "application/json")
+			.auth(authValid.username, authValid.password)
+			.send({
+				"firstname": updateBooking3.firstname,
+				"lastname": updateBooking3.lastname
+			})
+			.end((err, res) => {
+				expect(res).to.have.status(405);
+				expect(res).to.have.header("Content-Type", "text/plain; charset=utf-8");
+				expect(res).to.have.header("connection", "close");
+				expect(res.body).to.be.an("object").that.is.empty;
+				expect(res).to.have.property("error").to.have.property("text").to.equal("Method Not Allowed");
+				done();
+			});
+	});
 });
-
-
-// json
-// xml
-// valid
-// invalid 
-// malformed
